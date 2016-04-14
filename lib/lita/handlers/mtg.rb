f6b77c8c1abcb079ff30f29d7942cbb650b2c443
Luke Lancaster <luketlancaster@gmail.com>
2016-04-14 10:01:08 -0500

    returns images if there's one to find

--- lib/lita/handlers/mtg.rb
+++ lib/lita/handlers/mtg.rb
@@ -1,15 +1,40 @@
 require 'mtg_sdk'
+require 'pry'
 
 module Lita
   module Handlers
     class Mtg < Handler
       # insert handler code here
-      route(/mtg(?: me)?/i, :handle_trivia_request, command: true, help: {
-        'trivia me' => 'asks for a question from the trivia API'
+      attr_accessor :card
+
+      route(/mtg\s+(.*)?/i, :go_get_card, command: true, help: {
+        'mtg me' => 'asks for a card'
       })
 
-      def handle_trivia_request(req)
-        print "Here is a thing\n"
+
+
+      def go_get_card(req)
+        puts "Searching for #{req.match_data[1]}"
+        cards = MTG::Card.where(name: "#{req.match_data[1]}").all
+        if cards.empty?
+            return req.reply "Nothing found, please try again"
+        end
+
+        for card in cards
+            if card_good?(card)
+                @card = card
+                break
+            end
+        end
+
+        image = @card.image_url
+        req.reply image
+      end
+
+
+      def card_good?(card)
+        return true if card.image_url
+        false
       end
 
       Lita.register_handler(self)
